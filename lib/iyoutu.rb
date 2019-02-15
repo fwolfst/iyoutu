61f9c0cf5d7c68575e4790427e99448f079e24ef
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-02-15 14:42:15 +0100

    add ommitted gemspec and lib

--- lib/iyoutu.rb
+++ lib/iyoutu.rb
@@ -1,6 +1,28 @@
-require "iyoutu/version"
+require 'iyoutu/version'
+require 'iyoutu/youtube_dl_formats'
 
 module Iyoutu
   class Error < StandardError; end
-  # Your code goes here...
+
+  ABORT_CHOICE = {
+    name: "! Abort",
+    value: :abort
+  }
+
+  def self.user_choose_format(prompt, question, formats)
+    choices = formats.map do |format|
+      {
+        name:  "#{format.extension}/#{format.resolution_note}",
+        value: format.format_code
+      }
+    end
+    
+    choices << ABORT_CHOICE
+
+    prompt.select(question,
+                  choices,
+                  echo: false,
+                  filter: true,
+                  per_page: choices.length)
+  end
 end
